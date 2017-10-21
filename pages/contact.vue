7f79b5d799a86c4a25a583fb73cc5e7734b13a7d
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-10-21 17:32:59 +0100

    add contact page

---
+++ pages/contact.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">Contact Me</h1>
+      <p>
+        You can follow me on Twitter: <a href="https://twitter.com/ammezie" target="_blank">@ammezie</a>
+      </p>
+    </div>
+  </section>
+</template>
