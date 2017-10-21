ce142d474689d4432165ac40cb5d25330c00905b
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-10-21 16:26:21 +0100

    create navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/">Portfolio</a>
+
+        <button class="button navbar-burger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <a class="navbar-item" href="/">Home</a>
+          <a class="navbar-item" href="/about">About</a>
+          <a class="navbar-item" href="/projects">Projects</a>
+          <a class="navbar-item" href="/contact">Contact</a>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
