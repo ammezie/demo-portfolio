f0ffcea3fa9f79c27786cb806fa38094c4896b6e
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-10-21 17:17:09 +0100

    add about page

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <p>
+        My name is Chimezie Enyinnaya (AKA mezie), I’m a self taught software developer based in Lagos, Nigeria. I build 👨‍💻 modern applications for the web! I'm a technical writer, I write ✍🏾  technical articles and tutorials for various platforms including <a href="https://scotch.io/@mezie">Scotch.io</a>.
+      </p>
+    </div>
+  </section>
+</template>
